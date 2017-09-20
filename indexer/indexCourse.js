0a1963a650380e6e9ae3fb72307e21c366f82d87
Zachary Williams <wil11063@byui.edu>
2017-09-20 16:56:11 -0600

    Added moar documentation in the files

--- indexer/indexCourse.js
+++ indexer/indexCourse.js
@@ -1,25 +1,23 @@
-const Zip = require('adm-zip');
+/* Put dependencies here */
+const fs = require('fs');
 
 module.exports = (course, stepCallback) => {
   try {
-    /* Code Body */
 
-    var zipFile = new Zip(course.info.originalFilepath);
-    // DOES ADM ZIP OVERWITE STUFF? HOW DOES WORK?
-    zipFile.extractAllTo('../D2LReady/' + course.info.fileName, false);
+    // Use fs to read entire course structure
 
     /* This line must be included in every child module. Change "moduleName"
     to match the name of this module. The success message should be included
     for each item that is fixed/changed/altered/captainamerica'd. Please be
     specific in the message. */
-    course.report.moduleLogs['unzip.js'].changes.push('Successly unzipped' +
-                                                      course.info.fileName);
+    course.report.moduleLogs['indexCourse.js']
+    .changes.push('Successfully indexed ' + course.info.courseName);
 
     // On completion, return the course object back to its parent module.
     stepCallback(null, course);
   } catch (e) {
     /* If we have an error, throw it back up to its parent module.
     YOU MUST replace "moduleName" with the name of this module. */
-    stepCallback(new Error('unzip.js'), course);
+    stepCallback(new Error('indexCourse.js'), course);
   }
 };
--- preImport/preImport.js
+++ preImport/preImport.js
@@ -1,4 +1,25 @@
-module.exports = function(callback) {
-  console.log('ran');
-  callback(null);
+/* Require any dependencies here */
+const async = require('async');
+// Fix module...
+// Fix module...
+// Fix module...
+
+/* Our main function, called by main.js*/
+module.exports = (course, mainCallback) => {
+
+  /* List child modules in order of of operation */
+  const childModules = [
+    async.constant(course),
+  ];
+
+  async.waterfall(childModules, (err, resultCourse) => {
+    if (err) {
+      // If we have an error, send it up to main.js
+      mainCallback(err, resultCourse);
+    } else {
+      // If successful, return the course to main.js
+      mainCallback(null, resultCourse);
+    }
+  });
+
 };
