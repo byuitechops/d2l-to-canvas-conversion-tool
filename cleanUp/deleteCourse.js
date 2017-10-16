66354cdbf1848f7095904bdb96047182f8252c5e
Danverde <gre12014@byui.edu>
2017-10-16 14:45:45 -0600

    fixes to canvas wrapper and completed deleteCourse.js

--- cleanUp/deleteCourse.js
+++ cleanUp/deleteCourse.js
@@ -3,46 +3,30 @@
 /* Part of clean up. This module deletes the course 
  out of canvas if a fatal error was thrown */
 
-/* Put dependencies here */
 const canvas = require('../canvas.js');
 
-function work(course, stepCallback) {
-   //course.addModuleReport('deleteCourse');
-   /* Don't run if debug flag is on */
-   if (course.settings.debug === true) {
-      //      /course.success('deleteCourse', 'deleteCourse determined the course did not need to be deleted.');
+module.exports = (course, stepCallback) => {
+   course.addModuleReport('deleteCourse');
+   /* Don't run if delete course flag is on */
+   if (!course.settings.deleteCourse) {
+      course.success('deleteCourse', 'deleteCourse determined the course did not need to be deleted.');
       stepCallback(null, course);
       return;
    }
 
 
    /* Delete Course Here */
-   var url = `/api/v1/courses/${course.info.canvasCourseOU}`;
+   var url = `/api/v1/courses/${course.info.canvasCourseOU}?event=delete`;
    canvas.delete(url, (err, response) => {
       if (err) {
-         //course.throwErr('deleteCourse', err);
+         course.throwErr('deleteCourse', err);
          return;
       } else if (response.statusCode != 200) {
-         //course.throwErr('deleteCourse', new Error(`Status Code: ${response.statusCode}`));
+         course.throwErr('deleteCourse', new Error(`Status Code: ${response.statusCode}`));
          stepCallback(null, course);
          return;
       }
-      //course.success('deleteCourse', 'deleteCourse successfully deleted the course from Canvas');
+      course.success('deleteCourse', 'deleteCourse successfully deleted the course from Canvas');
       stepCallback(null, course);
    });
 }
-
-
-work({
-   info: {
-      canvasCourseOU: '322'
-   },
-   settings: {
-      debug: false
-   }
-}, (err, course) => {
-   if (err)
-      console.error(err);
-   else
-      console.log('done');
-});
