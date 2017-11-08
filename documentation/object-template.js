6994f9fd8148d1f2e252776608dc38a73884386f
Zachary Williams <wil11063@byui.edu>
2017-11-08 13:30:24 -0700

    Updated doc for object template

--- documentation/object-template.js
+++ documentation/object-template.js
@@ -1,21 +1,23 @@
 module.exports = {
-    'report': {
-        'moduleName': {
-            'fatalErrs': [],
-            'errors': [],
-            'changes': [],
+    'report': {                 /* Stores a report object for each child module */
+        'moduleName': {         // One of these is created for each child module
+            'fatalErrs': [],    // Stores any recorded fatal errors in a module report (only used in step modules)
+            'errors': [],       // Stores any recorded errors in a module report
+            'changes': [],      // Stores any successful changes caused by the module of a report
+            'warnings': []      // Stores recorded warnings that occurred while a module is running
         }
     },
-    'info': {
-        'migrationID': '',
-        'migrationIssues': [],
-        'fileName': '',
-        'originalFilepath': '',
-        'unzippedFilepath': '',
-        'preparedFilepath': '',
-        'courseName': '',
-        'canvasOU': 0,
-        'D2LOU': 0
+    'info': {   /* Stores various information bits that are used by the program, but are not determined by the user */
+        'migrationID': '<string>',          // ID for the course import migration
+        'migrationIssues': [],              // Any issues during course import are saved here
+        'fileName': '<string>',             // Name of the downloaded course zip
+        'originalFilepath': '<string>',     // Absolute filepath to Where the original downloaded zip is
+        'unzippedFilepath': '<string>',     // Absolute filepath to where the course is unzipped to
+        'altUnzippedFilepath': '<string>',  // Absolute filepath to where the course files are written to after preImport
+        'zippedFilepath': '<string>',       // Absolute filepath to where the unzipped course is zipped to
+        'courseName': '<string>',           // Course name ("FDREL 121 Reference - blah blah")
+        'canvasOU': 0,                      // Canvas ID for the newly created canvas course
+        'D2LOU': 0                          // D2L OU number
     },
     'settings': {
         // Determines what kind of output we have
@@ -29,14 +31,14 @@ module.exports = {
         // determines if files generated in process should be kept
         'keepFiles': false
     },
-    'content': [{
-        "name": '',
-        "newPath": '',
-        "oldPath": '',
-        "ext": '',
-        "dom": 'DOM',
-        "isTextFile": false
+    'content': [{               /* ALL files within a course are stored here as a flat array of file objects */
+        "name": '<string>',     // The file's name (i.e. "imsmanifest.xml")
+        "path": '<string>',     // Absolute path to the file's location on the hard drive
+        "ext": '<string>',      // File Extension (i.e. ".html")
+        "dom": '<string>',      // A STRING of the file's contents (must be parsed into a dom)
+        "canEdit": true         // Whether or not the file can be edited (binary files)
     }]
 
-    /* Functions available, check https://github.com/byuitechops/d2l-to-canvas-conversion-tool/blob/master/documentation/classFunctions.md */
+    /* To see what functions are available to use from the course object:
+    https://github.com/byuitechops/d2l-to-canvas-conversion-tool/blob/master/documentation/classFunctions.md */
 };
