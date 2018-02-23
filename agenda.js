e00d7e02590c3ef72b234613565c97f5135e6236
Zachary Williams <wil11063@byui.edu>
2018-02-23 10:04:10 -0700

    Updated package list

--- agenda.js
+++ agenda.js
@@ -34,6 +34,8 @@ const createHomepage = require('create-homepage');
 const webFeaturesUpdate = require('web-features-update');
 const deleteUnwantedAssignments = require('assignments-delete-unwanted');
 const publishSettings = require('module-publish-settings');
+const courseSettings = require('course-settings');
+const actionSeries = require('action-series-master');
 
 /* CleanUp */
 const removeFiles = require('remove-files');
@@ -93,6 +95,8 @@ exports.postImport = [
     webFeaturesUpdate, // REQUIRED FOR ONLINE - Creates and removes specific html for online styling
     deleteUnwantedAssignments, // REQUIRED FOR ONLINE - Removes [CO#] assignments from the course
     publishSettings, // DEFAULT REQUIRED - Publishes/ Unpublishes items according to their settings in D2L
+    courseSettings, // REQUIRED FOR ONLINE - Sets the course settings as written in this module's documentation
+    actionSeries, // REQUIRED FOR ONLINE - Runs action series model
 ];
 
 exports.cleanUp = [
